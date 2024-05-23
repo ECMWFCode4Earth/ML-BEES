20d30b824e0db4ec92f013b8e2ccc29c96973740
tfohrmann <till.fohrmann@posteo.de>
2024-05-23 20:44:18 +0000

    Added spatial mean

--- ML-BEES-eval/eval_utils.py
+++ ML-BEES-eval/eval_utils.py
@@ -21,6 +21,31 @@ def bias(mod, ref, vars, relative=False):
     if relative:
         #Normalize bias using the central residual mean square of the reference data:
         crms = np.sqrt( (( ref.data.sel(variable=vars) - ref.global_data_means.sel(variable=vars) )**2).mean(dim="time") )
+        
         return( np.abs(bias)/crms )
     else:
         return( bias )
+    
+
+def spatial_mean(ds, vars, weights=None):
+    """
+    Computes the (weighted) spatial mean for selected variables `vars``
+    on dataset `ds`.
+
+    See doi.org/10.1029/2018MS001354 for details.
+
+    --- Parameters ---
+    ds:         xarray.Dataset
+    vars:       str or iterable of str
+    weights:    np.array
+
+    --- Returns ---
+    xarray.Dataset
+    """
+    if weights is None:
+        weights = np.ones(ds.dims["x"]) #uniform weights
+
+    total_weighted_area = (weights * ds.clim_data.sel(clim_variable="clim_cell_area")).sum(dim="x") 
+    spatial_integral = (weights * ds.data.sel(variable="e") * ds.clim_data.sel(clim_variable="clim_cell_area")).sum(dim="x")
+
+    return( spatial_integral/total_weighted_area )
