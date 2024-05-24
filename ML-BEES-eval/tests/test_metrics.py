bbbb2ddda735b545fa2cfd377d8cf1f4a62e46e3
tfohrmann <till.fohrmann@posteo.de>
2024-05-24 12:55:22 +0000

    Add test of relative bias

--- ML-BEES-eval/tests/test_metrics.py
+++ ML-BEES-eval/tests/test_metrics.py
@@ -14,7 +14,7 @@ from eval_utilities import metrics
 class TestBias(unittest.TestCase):
     def test_expected_bias(self):
         """
-        Test that function returns expected bias on mock datasets
+        Test that `metrics.bias` returns expected bias on mock datasets
         """
 
         expected_bias = 1.
@@ -50,6 +50,48 @@ class TestBias(unittest.TestCase):
         # Test:
         np.testing.assert_allclose(expected_bias, metrics.bias(ds_mod, ds_ref, vars=variables))
 
+
+    def test_relative_bias(self):
+        """
+        Test that `metrics.bias`'s relative error functionality works on mock datasets
+        """
+
+        expected_bias = 1.
+        ref_scale = 3.1415 #see assertion below
+
+        # Define dimensions:
+        lats = np.arange(60.0, 60.1, step=0.1)
+        lons = np.arange(20.0, 20.1, step=0.1)
+        time = np.arange(np.datetime64("2010-02-01T00", "ns"),
+                        np.datetime64("2012-02-01T00", "ns"), 
+                        np.timedelta64(6,"h"))
+        variables = ["Bees", "Supercalifragilisticexpialidocious"]
+
+        # Generate data:
+        np.random.seed(0)
+        data_ref = np.random.normal(scale=ref_scale, size=(len(time), len(lats), len(variables)))
+        data_mod = expected_bias + data_ref
+
+        # Create datasets:
+        ds_mod = xr.Dataset(
+            data_vars=dict(data=(["time", "x", "variable"], data_mod),
+                        global_data_means=(["x", "variable"], np.mean(data_mod, axis=0))),
+            coords=dict(lat=("x", lats), lon=("x", lons), time=("time", time), 
+                        variable=("variable", variables), x=("x", np.arange(len(lats))))
+        )
+
+        ds_ref = xr.Dataset(
+            data_vars=dict(data=(["time", "x", "variable"], data_ref),
+                        global_data_means=(["x", "variable"], np.mean(data_ref, axis=0))),
+            coords=dict(lat=("x", lats), lon=("x", lons), time=("time", time), 
+                        variable=("variable", variables), x=("x", np.arange(len(lats))))
+        )
+
+        # Test:
+        # Since `data_ref` is normally distributed with scale `ref_scale`, the normalization is expected to
+        # be 1/`ref_scale`. The expected relative error is then `expected_bias`/`ref_scale`.
+        abs_diff = np.abs(metrics.bias(ds_mod, ds_ref, variables, relative=True) - expected_bias/ref_scale)
+        self.assertTrue( (abs_diff < 0.1).all().item() )
 
 
 
